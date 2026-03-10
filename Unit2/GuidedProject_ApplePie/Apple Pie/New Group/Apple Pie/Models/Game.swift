b14fac4636e51060a6d6057d6c4805fd558ae999
MiguelGaido <gaidomigueljr@gmail.com>
2026-03-10 00:18:56 -0400

    update

---
+++ Unit2/GuidedProject_ApplePie/Apple Pie/New Group/Apple Pie/Models/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Miguel Gaido on 3/10/26.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
+
